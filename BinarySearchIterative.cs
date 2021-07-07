548ae9bf685a340a9785a5171746140eae47889d
abasheva <72603461+abasheva@users.noreply.github.com>
2021-07-07 09:37:19 +0300

    Add files via upload

---
+++ BinarySearchIterative.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class BinarySearchIterative
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+
+            int searchedNumber = int.Parse(Console.ReadLine());
+            int indexSearchedNumber = BinarySearch(arr, searchedNumber);
+
+            Console.WriteLine(indexSearchedNumber);
+        }
+
+        static int BinarySearch(int[] arr, int searchNumber)
+        {
+            int leftIndex = 0;
+            int rightIndex = arr.Length - 1;
+
+            while (leftIndex <= rightIndex)
+            {
+                int middleIndex = (leftIndex + rightIndex) / 2;
+
+                if (arr[middleIndex] == searchNumber)
+                    return middleIndex;
+                else if (arr[middleIndex] < searchNumber)
+                    leftIndex = middleIndex + 1;
+                else
+                    rightIndex = middleIndex - 1;
+
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ BinarySearchRecursive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class BinarySearchRecursive
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+                arr[i] = int.Parse(Console.ReadLine());
+
+            int searchedNumber = int.Parse(Console.ReadLine());
+            int indexSearchedNumber = BinarySearchRec(arr, searchedNumber, 0, size - 1);
+
+            Console.WriteLine(indexSearchedNumber);
+        }
+
+        static int BinarySearchRec(int[] arr, int serchedNumber, int leftIndex, int rightIndex)
+        {
+            if(leftIndex <= rightIndex)
+            {
+                int middleIndex = (leftIndex + rightIndex) / 2;
+
+                if (arr[middleIndex] == serchedNumber)
+                    return middleIndex;
+                else if (arr[middleIndex] < serchedNumber)
+                    return BinarySearchRec(arr, serchedNumber, middleIndex + 1, rightIndex);
+                else
+                    return BinarySearchRec(arr, serchedNumber, leftIndex, middleIndex - 1);
+            }
+
+            return -1;
+        }
+    }
+}
